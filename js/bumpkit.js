048e20d402c3b114aa6e00ebe010d6ccbb65e41c
Brent Jackson <jacksonblack@gmail.com>
2014-08-03 17:56:10 -0400

    Sequencer clock with custom event dispatcher

--- js/bumpkit.js
+++ js/bumpkit.js
@@ -21,8 +21,12 @@ Bumpkit.prototype.speaker = this.context.destination;
 
 // Mixer
 
+
+Bumpkit.prototype.mixer = function() {};
+
 // Generic track object
-var Track = function() {
+// TO DO: Figure out the best way to implement this
+Bumpkit.Track = function() {
   this.input = self.context.createGain();
   this.mute = self.context.createGain();
   this.volume = self.context.createGain();
@@ -31,16 +35,13 @@ var Track = function() {
   this.mute.connect(this.volume);
 };
 
-Bumpkit.prototype.mixer = function() {
-};
-
 Bumpkit.prototype.mixer.tracks = [];
-Bumpkit.prototype.mixer.master = new Track();
-// Connect master to speaker
+Bumpkit.prototype.mixer.master = new Bumpkit.Track();
+// TO DO: Connect master to speaker
 //Bumpkit.prototype.mixer.master.volume.connect(this.speaker);
 
 Bumpkit.prototype.mixer.addTrack = function() {
-  var track = new Track();
+  var track = new Bumpkit.Track();
   track.volume.connect(this.master.input);
   this.tracks.push(track);
 };
@@ -50,8 +51,85 @@ Bumpkit.prototype.mixer.removeTrack = function(index) {
 };
 
 
+// Scheduler / Clock
+Bumpkit.prototype.tempo = 120;
+Bumpkit.prototype.stepDuration = (60/120) / 4;
+Bumpkit.prototype.nextStepTime = 0;
+Bumpkit.prototype.currentStep = 0;
+Bumpkit.prototype.isPlaying  = false;
+Bumpkit.prototype.loopLength = null;
+Bumpkit.prototype.timerID = 0;
+
+
+//Bumpkit.prototype.step = new CustomEvent('step', function() {
+//  return { detail: { step: this.currentStep, test: 'derp' } };
+//});
+Bumpkit.prototype.step = new CustomEvent('step', { detail: {} });
+
+Bumpkit.prototype.scheduleStep = function(when) {
+  if (!this.context.currentTime) { // Start the clock
+    var dummySource = this.context.createBufferSource();
+  }
+  this.step.detail.step = this.currentStep;
+  var self = this;
+  window.dispatchEvent(self.step);
+};
+
+Bumpkit.prototype.scheduler = function() {
+  var self = this,
+      scheduleAhead = .1,
+      lookahead = 25;
+
+  while (this.nextStepTime < this.context.currentTime + scheduleAhead) {
+    this.scheduleStep(this.nextStepTime);
+    this.nextStepTime += this.stepDuration;
+    this.currentStep++;
+  };
+  this.timerID = setTimeout(function() { self.scheduler() }, lookahead);
+};
+
+Bumpkit.prototype.stop = function() {
+  var self = this;
+  this.isPlaying = false;
+  window.clearTimeout(self.timerID);
+};
+
+Bumpkit.prototype.playPause = function() {
+  if (!this.isPlaying) {
+    this.currentStep = 0;
+    this.nextStepTime = this.context.currentTime;
+    this.scheduler();
+    this.isPlaying = !this.isPlaying;
+  } else {
+    this.stop();
+  }
+};
+
+//
+// Trigger playback, pass source with buffer, and output node
+Bumpkit.prototype.trigger = function(source, when, output, options) {
+  source.connect(output);
+  source.start(when, options.offset || 0);
+  if (options.duration) {
+    source.stop(when + options.duration);
+  }
+};
+
+
+
+// Sampler
+  // loadSample
+  // offset
+  // duration
+  // envelopeNode
+  // ASDR
+  // Pitch
+
+
+
 // Bootstrap and connect the mixer
 Bumpkit.prototype.init = function() {
+  // TO DO: Figure out best implementation
   // Connect master channel to speaker
   this.mixer.master.volume.connect(this.speaker);
 };
@@ -59,10 +137,16 @@ Bumpkit.prototype.init = function() {
 
 
 ////////////////////////////////
-// Debugging
+// Debugging and testing
 var test = new Bumpkit();
 test.init();
 test.mixer.addTrack();
-console.log(test.mixer.master);
-console.log(test.mixer.tracks);
+test.mixer.addTrack();
+console.log('master and tracks ', test.mixer.master, test.mixer.tracks);
+test.mixer.removeTrack(0);
+console.log('removed track 0', test.mixer.tracks);
+console.log('tempo and stepDuration:', test.tempo, test.stepDuration);
+window.addEventListener('step', function(e) {
+  console.log('test step', e.detail.step);
+});
 

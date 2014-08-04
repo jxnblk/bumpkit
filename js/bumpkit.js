e555fff181b57349c7badef8946de9fe3beee5a7
Brent Jackson <jacksonblack@gmail.com>
2014-08-03 20:53:16 -0400

    Basic metronome stuff

--- js/bumpkit.js
+++ js/bumpkit.js
@@ -1,60 +1,51 @@
+// Bumpkit
+//
+// TO DO:
+// - add offline audio context for rendering
+// - add return tracks to mixer
+
 
 'use strict';
 
 console.log('Bumpkit');
-  try {
-    this.context = new (window.AudioContext || window.webkitAudioContext)();
-  }
-  catch(e) {
-    console.error('Web Audio API not supported in this browser.');
-  }
+try { var testcontext = new (window.AudioContext || window.webkitAudioContext)(); }
+catch(e) { console.error('Web Audio API not supported in this browser.'); }
 
-var Bumpkit = function() {};
-//Bumpkit.self = this;
+var Bumpkit = function() {
+  this.self = this;
+  this.context = new(window.AudioContext || window.webkitAudioContext)();
+  this.speaker = this.context.destination;
 
-// Initialize context
-Bumpkit.prototype.context = new(window.AudioContext || window.webkitAudioContext)();
-
-// Speaker Destination
-Bumpkit.prototype.speaker = this.context.destination;
-// Add offline audio context for renderer
-
-
-// Mixer
-
-
-Bumpkit.prototype.mixer = function() {};
-
-// Generic track object
-// TO DO: Figure out the best way to implement this
-Bumpkit.Track = function() {
-  this.input = self.context.createGain();
-  this.mute = self.context.createGain();
-  this.volume = self.context.createGain();
-  // Add effects chain array and compressor node
-  this.input.connect(this.mute);
-  this.mute.connect(this.volume);
+  // Mixer
+  this.mixer = {};
+  this.mixer.master = {};
+    this.mixer.master.input = this.context.createGain();
+    this.mixer.master.mute = this.context.createGain();
+    this.mixer.master.volume = this.context.createGain();
+    this.mixer.master.input.connect(this.mixer.master.mute);
+    this.mixer.master.mute.connect(this.mixer.master.volume);
+    this.mixer.master.volume.connect(this.speaker);
+  this.mixer.tracks = [];
 };
 
-Bumpkit.prototype.mixer.tracks = [];
-Bumpkit.prototype.mixer.master = new Bumpkit.Track();
-// TO DO: Connect master to speaker
-//Bumpkit.prototype.mixer.master.volume.connect(this.speaker);
-
-Bumpkit.prototype.mixer.addTrack = function() {
-  var track = new Bumpkit.Track();
-  track.volume.connect(this.master.input);
-  this.tracks.push(track);
+Bumpkit.prototype.addTrack = function() {
+  var track = {};
+  track.input = this.context.createGain();
+  track.mute = this.context.createGain();
+  track.volume = this.context.createGain();
+  track.input.connect(track.mute);
+  track.mute.connect(track.volume);
+  track.volume.connect(this.mixer.master.input);
+  this.mixer.tracks.push(track);
 };
 
-Bumpkit.prototype.mixer.removeTrack = function(index) {
-  this.tracks.splice(index, 1);
+Bumpkit.prototype.removeTrack = function(index) {
+  this.mixer.tracks.splice(index, 1);
 };
 
 
 // Scheduler / Clock
 Bumpkit.prototype.tempo = 120;
-//Bumpkit.prototype.stepDuration = (60/120) / 4;
 Bumpkit.prototype.stepDuration = function() {
   return (60/this.tempo) / 4;
 };
@@ -63,11 +54,6 @@ Bumpkit.prototype.currentStep = 0;
 Bumpkit.prototype.isPlaying  = false;
 Bumpkit.prototype.loopLength = null;
 Bumpkit.prototype.timerID = 0;
-
-
-//Bumpkit.prototype.step = new CustomEvent('step', function() {
-//  return { detail: { step: this.currentStep, test: 'derp' } };
-//});
 Bumpkit.prototype.step = new CustomEvent('step', { detail: {} });
 
 Bumpkit.prototype.scheduleStep = function(when) {
@@ -77,6 +63,7 @@ Bumpkit.prototype.scheduleStep = function(when) {
   this.step.detail.step = this.currentStep;
   this.step.detail.when = when;
   var self = this;
+  console.log('step',this.currentStep);
   window.dispatchEvent(self.step);
 };
 
@@ -121,15 +108,13 @@ Bumpkit.prototype.trigger = function(source, when, output, options) {
   console.log(when, when + options.duration);
   source.start(when, options.offset || 0);
   if (options.duration) {
-    console.log(options.duration);
     source.stop(when + options.duration);
   }
 };
 
 
 // Beep Subclass
-// This might not be the right approach
-// Simple sine oscillator for metronome
+// This might not be the right approach // Simple sine oscillator for metronome
 var Beep = function() {
   Bumpkit.call(this);
   var self = this;
@@ -144,17 +129,15 @@ var Beep = function() {
 };
 Beep.prototype = Object.create(Bumpkit.prototype);
 Beep.prototype.contructor = Bumpkit;
-Beep.prototype.output = 0;
 Beep.prototype.duration = .0625;
 Beep.prototype.pattern = [1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0];
+Beep.prototype.output = 0;
 Beep.prototype.play = function(when) {
   var osc = this.context.createOscillator();
   osc.type = 0;
   osc.frequency.value = 200;
-  this.trigger(osc, when, this.context.destination, { duration: this.duration });
+  this.trigger(osc, when, this.output, { duration: this.duration });
 };
-// TO DO: Figure out how to share the same context
-  // TO DO: Check for latency problems with event listener //console.log(this.context.currentTime, when);
 // Beep.prototype.envelopeNode;
 
 // Sampler
@@ -166,12 +149,11 @@ Beep.prototype.play = function(when) {
   // Pitch
 
 
-
 // Bootstrap and connect the mixer
 Bumpkit.prototype.init = function() {
   // TO DO: Figure out best implementation
   // Connect master channel to speaker
-  this.mixer.master.volume.connect(this.speaker);
+  //  this.mixer.master.volume.connect(this.speaker);
 };
 
 
@@ -179,29 +161,13 @@ Bumpkit.prototype.init = function() {
 ////////////////////////////////
 // Debugging and testing
 var test = new Bumpkit();
-test.init();
-test.mixer.addTrack();
-test.mixer.addTrack();
-console.log('master and tracks ', test.mixer.master, test.mixer.tracks);
-test.mixer.removeTrack(0);
-console.log('removed track 0', test.mixer.tracks);
-console.log('tempo and stepDuration:', test.tempo, test.stepDuration());
-test.tempo = 144;
-console.log('change tempo and stepDuration:', test.tempo, test.stepDuration());
-
-// Set 16 step loop
+console.log(test.context === test.mixer.master.volume.context);
+test.addTrack();
+test.addTrack();
 test.loopLength = 16;
-
-// Listen for step in metronome
-window.addEventListener('step', function(e) {
-  //console.log('test step', e.detail.step);
-});
-
-// Beep
+//console.log(test.context === test.mixer.tracks[0].volume.context);
 var beep = new Beep();
+beep.context = test.context; // Figure out how to negate the need for this
+//console.log(test.context === beep.context);
 beep.output = test.mixer.tracks[0].input;
-beep.context = test.context;
-//console.log(beep);
-console.log(beep.output.context === test.context);
-//console.log(beep.output);
 

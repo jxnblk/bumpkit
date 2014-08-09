6a9afd12e88f47e24d31d2bc3342e956e93f5e67
Brent Jackson <jacksonblack@gmail.com>
2014-08-09 15:59:34 -0400

    messing with stuff

--- js/bumpkit.js
+++ js/bumpkit.js
@@ -1,12 +1,14 @@
 // Bumpkit
 // Main audio context with decorators
 
-var AC = window.AudioContext;
+var AC = window.AudioContext || window.webkitAudioContext;
 
 var Bumpkit = function() {};
 Bumpkit.prototype = new AC();
 
 Bumpkit.prototype.arm = 'prop';
+console.log(AC);
+Bumpkit.prototype.createGain = AC.createGain();
 
 
 

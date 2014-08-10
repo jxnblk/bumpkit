4b8f0a2884e6f8d3863f8e277ad346b1bc255a31
Brent Jackson <jacksonblack@gmail.com>
2014-08-10 16:30:08 -0400

    Markdown page generation

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,14 @@ var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+
 var uncss = require('gulp-uncss');
 var minifyCss = require('gulp-minify-css');
+var markdown = require('gulp-markdown');
+var pygmentize = require('pygmentize-bundled');
+var header = require('gulp-header');
+var footer = require('gulp-footer');
+var fs = require('fs');
 
 gulp.task('compile', function() {
   return gulp.src('src/bumpkit.js')
@@ -28,10 +34,33 @@ gulp.task('uncss', function() {
     .pipe(gulp.dest('docs/css'));
 });
 
+
+// Create homepage based on README.md
+gulp.task('md', function() {
+
+  var mdoptions = {
+    highlight: function (code, lang, callback) {
+      pygmentize({ lang: lang, format: 'html' }, code, function(err, result) {
+        callback(err, result.toString());
+      });
+    }
+  };
+
+  return gulp.src('README.md')
+    .pipe(markdown(mdoptions))
+    .pipe(rename('index.html'))
+    .pipe(header( fs.readFileSync('./docs/_header.html', 'utf8')))
+    .pipe(footer( fs.readFileSync('./docs/_footer.html', 'utf8') ))
+    .pipe(gulp.dest('.'));
+
+});
+
+
 gulp.task('default', ['compile', 'minify'], function() {
 });
 
+
 gulp.task('watch', function() {
-  gulp.watch('src/*', ['compile', 'minify']);
+  gulp.watch('src/*', ['compile', 'minify', 'md']);
 });
 
